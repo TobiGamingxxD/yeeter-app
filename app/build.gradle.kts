31d024161717d25e0ff272bb6c0a64cb5056101c
TobiGaming <tobias.rath@e-mail.de>
2023-11-20 16:16:38 +0100

    Update V.0.13
    Fixed:
    - first release apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tobigaming.yeeter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.12"
+        versionCode = 4
+        versionName = "0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
